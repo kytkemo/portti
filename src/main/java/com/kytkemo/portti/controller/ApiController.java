5bdefa5ab0613865cfe459cde040af47305ee0ee
Kasper Hirvikoski <kasper.hirvikoski@gmail.com>
2013-10-28 12:08:36 +0200

    Separate import groups

--- src/main/java/com/kytkemo/portti/controller/ApiController.java
+++ src/main/java/com/kytkemo/portti/controller/ApiController.java
@@ -2,6 +2,7 @@ package com.kytkemo.portti.controller;
 
 import java.util.HashMap;
 import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
