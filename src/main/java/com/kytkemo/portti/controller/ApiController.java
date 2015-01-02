9c9e7ac5b0a320bfed1f463910feeed02701415b
Kasper Hirvikoski <kasper.hirvikoski@gmail.com>
2015-01-02 12:57:34 +0200

    Final class

--- src/main/java/com/kytkemo/portti/controller/ApiController.java
+++ src/main/java/com/kytkemo/portti/controller/ApiController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("api")
-public class ApiController {
+public final class ApiController {
 
     @RequestMapping(method = RequestMethod.GET, value = "secret", produces = "application/json")
     @ResponseBody
--- src/main/java/com/kytkemo/portti/controller/BaseController.java
+++ src/main/java/com/kytkemo/portti/controller/BaseController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class BaseController {
+public final class BaseController {
 
     @RequestMapping(method = RequestMethod.GET, value = "/")
     public String getRoot() {
